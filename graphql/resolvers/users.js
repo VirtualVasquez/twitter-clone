ee47eb72d3682611ded906c81b419ab376139e03
VirtualVasquez <mxv647@med.miami.edu>
2020-11-27 15:37:56 -0500

    Register and Login Users created, checkpoint 1:12:50

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,27 +1,92 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const{ UserInputError} = require('apollo-server');
+
+const {validateRegisterInput, validateLoginInput} = require('../../util/validators')
 
 const { SECRET_KEY } = require('../../config')
 const User = require('../../models/User');
 
+function generateToken(user){
+    return jwt.sign({
+        id: user.id, 
+        email: user.email,
+        username: user.username
+    }, SECRET_KEY, { expiresIn: '1h'}); 
+}
 
 module.exports = {
     Mutation: {
+        async login(_, {username, password}){
+            const {errors, valid} = validateLoginInput(username, password);
+            const user = await User.findOne({username});
+
+            //if valid is false
+            if(!valid){
+                throw new UserInputError('Errors', {errors})
+            }
+            //if user not registered
+            if(!user){
+                errors.general = 'User not found';
+                throw new UserInputError('Wrong credentials', {errors})
+            }
+            //if password is incorrect
+            const match = await bcrypt.compare(password, user.password);
+            if(!match){
+                errors.general = 'Wrong credentials';
+                throw new UserInputError('Wrong credentials', {errors})
+            }
+
+            const token = generateToken(user)
+
+            return{
+                ...user._doc,
+                id: user._id,
+                token
+            }
+        },
+        
+        
+        
+        //four options in the resolver arguments
+            //parent - result of last step (no last step here)
+            //args - user sent data (destructured here for ease)
+            //context - not being used, so not included
+            //info - not being used, so not included
+            //general - metadata. Not being used
+
         async register(
             _, 
             {
-                registerInput: {username, email, password, confirmPassword}
-            }, 
-            context, 
-            info
+                registerInput: {username, email, password, confirmPassword} 
+            }
             ){
-            //TODO: Validate user data
-            //TODO: Make sure user doesn't already exist
-            //TODO: has password and create an auth token
+            //TODO: Validate user data================
+            const { valid, errors } = validateRegisterInput(
+                username, 
+                email, 
+                password, 
+                confirmPassword
+            );
+            if(!valid){
+                throw new UserInputError('Errors', { errors});
+            }
+            //========================================
 
+            //TODO: Make sure user doesn't already exist=====            
+            const user = await User.findOne({username});
+            if(user){
+                throw new UserInputError('Username is taken', {
+                    errors:{
+                        username: 'This username is taken'
+                    }
+                })
+            }
+            //========================================
+            
+            //TODO: has password and create an auth token======
             //this will take the password and has it 12 times
             password = await bcrypt.hash(password, 12);
-
             const newUser = new User({
                 email,
                 username,
@@ -30,18 +95,17 @@ module.exports = {
             });
             //save to the DB
             const res = await newUser.save();
-             
-            const token = jwt.sign({
-                id: res.id, 
-                email: res.email,
-                username: res.username
-            }, SECRET_KEY, { expiresIn: '1h'});
 
+            //create token for the user
+                //jwt.sign() takes data to put inside the token
+                //```SECRET_KEY``` encodes our token, allowing only our server to decode it
+            const token = generateToken(res)
             return{
                 ...res._doc,
                 id: res._id,
                 token
             }
+            //==========================================
 
         }
     }
